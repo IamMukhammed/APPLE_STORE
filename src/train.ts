b7ebf0ce2b54543fa0f211a34b77764df300c7b8
IamMukhammed <abdullaevazizkhan@gmail.com>
2024-12-08 12:01:49 +0500

    feat: TASK - H

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,10 @@
 console.log("Hello World");
+
+
+function getPositive(arr: number[]): string {
+    return arr
+        .filter((num: number) => num > 0) // Faqat ijobiy sonlarni tanlab olish
+        .join(''); // Tanlangan sonlarni birlashtirish
+}
+
+console.log(getPositive([1, -4, 2])); // "12"

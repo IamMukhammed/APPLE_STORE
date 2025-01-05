8f82cd7ee8ddd50764c0dbfeca858b7743a49bf0
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-01-05 12:00:26 +0500

    feat: signup process frontend validation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -47,9 +47,11 @@ restaurantController.processSignup = async (
 ) => {
     try {
         console.log("processSignup");
+        console.log("req.body:", req.body);
         const file = req.file;
         console.log("file:", file);
-        if(!file) throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
+        if ( !file ) 
+            throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
 
         const newMember: MemberInput = req.body;
         newMember.memberImage = file?.path //.replace(/\\/g, "/");

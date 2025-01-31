b1248bc74fe53b16f141b419815179b1f6746506
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-01-31 12:25:53 +0500

    feat: Develop getPorducts rest API

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,9 +1,11 @@
 import Errors, { HttpCode } from "../libs/Error";
-import { Product, ProductInput, ProductUpdateInput } from "../libs/types/product";
+import { Product, ProductInput, ProductInquiry, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { memoryStorage } from "multer";
 import { Message } from "../libs/Error";
 import { shapeIntoMongooseObjectId } from "../libs/config";
+import { ProductStatus } from "../libs/enums/product.enum";
+import { T } from "../libs/types/common";
 
 class ProductService {
     private readonly productModel;
@@ -12,7 +14,36 @@ class ProductService {
     }
 
     /* SPA */
-    
+    public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
+        console.log("inquiry:", inquiry);
+        
+        const match: T = {productStatus: ProductStatus.PROCESS};
+        if (inquiry.productCollection) 
+            match.productController = inquiry.productCollection;
+
+        if (inquiry.search) {
+            match.productName = {$regex: new RegExp(inquiry.search, "i")};
+        }
+        
+        const sort: T = inquiry.order === "productPrice" 
+            ? { [inquiry.order]: 1 } // dynamic key ni xosil qilib beradi
+            : { [inquiry.order] : -1 };
+
+        
+        const result = await  this.productModel
+            .aggregate([
+                {$match: match},
+                {$sort: sort},
+                {$skip: (inquiry.page * 1 - 1) * inquiry.limit},    // 0
+                {$limit: inquiry.limit * 1},                        // 3
+            ])
+            .exec();
+        
+        if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+        return result;
+    }
+
     /* SSR */
     public async getAllProducts(): Promise<Product[]> {
         const result = await this.productModel.find().exec();
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
+import productController from "./controllers/product.controller";
 
 
 // router.get('/', memberController.goHome);
@@ -20,7 +21,8 @@ router.get("/member/top-users", memberController.getTopUsers);
 
 /* PRODUCTS */
 
-// router.post("/products", memberController.products);
+router.get("/product/all", productController.getProducts);
+
 
 /* ORDERS */
 

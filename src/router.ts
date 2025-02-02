05f1921284a0950bd90d66fc1a9729f65a02bade
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-02-02 11:23:07 +0500

    feat: Develop createOrder rest API

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 import memberController from "./controllers/member.controller";
 import uploader from "./libs/utils/uploader";
 import productController from "./controllers/product.controller";
+import orderController from "./controllers/order.controller";
 
 
 /* MEMBERS */
@@ -21,5 +22,6 @@ router.get("/product/:id", memberController.retrieveAuth, productController.getP
 
 
 /* ORDERS */
+router.post("/order/create", memberController.verifyAuth, orderController.createOrder);
 
 export default router;
--- src/schema/OrderItem.model.ts
+++ src/schema/OrderItem.model.ts
@@ -23,7 +23,7 @@ const orderItemSchema = new Schema(
         },
 
     }, 
-    { timestamps: true}
+    { timestamps: true }
 );
 
 export default mongoose.model("OrderItem", orderItemSchema);

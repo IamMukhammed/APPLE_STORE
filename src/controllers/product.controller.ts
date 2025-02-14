8523cdef78a96979d8511f37b133ca7132030842
IamMukhammed <abdullaevazizkhan@gmail.com>
2025-02-14 19:50:14 +0500

    fix: Modify getProducts page logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -12,23 +12,52 @@ const productController: T = {};
 
 /* SPA */
 
+// productController.getProducts = async (req: Request, res: Response) => {
+//     try {
+//         console.log("getProducts");
+//         const { page, limit, order, productCollection, search } = req.query;
+//         const inquiry: ProductInquiry = {
+//             order: String(order),
+//             page: Number(page),
+//             limit: Number(limit),
+//         };
+//         if (productCollection) {
+//             inquiry.productCollection = productCollection as ProductCollection;
+//         }
+//         if (search) inquiry.search = String(search);
+
+//         const result = await productService.getProducts(inquiry);
+//         console.log("result => ", result);
+
+//         res.status(HttpCode.OK).json(result);
+//     } catch (err) {
+//         console.log("Error, getProducts:", err);
+//         if (err instanceof Errors) res.status(err.code).json(err);
+//         else res.status(Errors.standard.code).json(Errors.standard);
+//     }
+// };
+
 productController.getProducts = async (req: Request, res: Response) => {
     try {
-        console.log("getProducts");
+        console.log("getProducts API chaqirildi!");
+        console.log("Query params:");
+
         const { page, limit, order, productCollection, search } = req.query;
         const inquiry: ProductInquiry = {
             order: String(order),
             page: Number(page),
             limit: Number(limit),
         };
+
         if (productCollection) {
             inquiry.productCollection = productCollection as ProductCollection;
         }
         if (search) inquiry.search = String(search);
 
+        console.log("Inquiry object:", inquiry); // 🛠 Tekshirish uchun qo‘shildi
         const result = await productService.getProducts(inquiry);
-        console.log("result=> ", result);
-
+        
+        console.log("API javobi:", result); // 🛠 Tekshirish uchun qo‘shildi
         res.status(HttpCode.OK).json(result);
     } catch (err) {
         console.log("Error, getProducts:", err);
